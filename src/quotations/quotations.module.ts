88d08fe1a852e46b0fcfce55c0e933dbd91aaa37
brenomachadodomonte <brenomachadodomonte@gmail.com>
2023-08-13 09:32:08 -0300

    Build: Criação do módulo de configuração

--- src/quotations/quotations.module.ts
+++ src/quotations/quotations.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { QuotationsService } from './quotations.service';
 import { QuotationsController } from './quotations.controller';
 import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+    }),
+    HttpModule,
+  ],
   providers: [QuotationsService],
   controllers: [QuotationsController],
 })
