a590c2a75917da1ed520174352f51d63f01eccc7
brenomachadodomonte <brenomachadodomonte@gmail.com>
2023-08-14 16:41:45 -0300

    Docs: Instalação do Swagger no caminho /api

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { QuotationsModule } from './quotations/quotations.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(QuotationsModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('API de Cotação')
+    .setDescription('API de Cotação que console cotações da API Frete Rápido')
+    .setVersion('1.0')
+    .addTag('Quotations')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   const port = 3000;
   await app.listen(port);
